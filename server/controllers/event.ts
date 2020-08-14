102a539f7ce35e66f00bba8629345c751adcb5ab
Juergen Skrotzky <JorgenVikingGod@gmail.com>
2020-08-14 21:29:39 +0200

    Add Scheduler data to events

--- server/controllers/event.ts
+++ server/controllers/event.ts
@@ -1,5 +1,5 @@
 import BaseCtrl from './base';
-import { CalendarEvent } from 'models/event';
+import { CalendarEvent, SchedulerDeviceType, SchedulerDevice } from './../models/event';
 
 class EventCtrl extends BaseCtrl {
   model = [
@@ -29,7 +29,48 @@ class EventCtrl extends BaseCtrl {
         freq: 'weekly',
         interval: 2,
       },
-      duration: '00:60',
+      duration: '01:00',
+    },
+    {
+      id: '3',
+      allDay: false,
+      title: 'smoke PC0815 with MEAS01 and GEN-TYPE-01',
+      backgroundColor: 'green',
+      textColor: 'yellow',
+      overlap: true,
+      rrule: {
+        dtstart: '2020-07-08T07:30:00',
+        byweekday: ['we'],
+        freq: 'weekly',
+        interval: 1,
+      },
+      duration: '01:30',
+      scheduler: {
+        usePC: true,
+        pc: new SchedulerDevice('PC0815', '1'),
+        device: new SchedulerDevice('MEAS01', '2'),
+        generator: new SchedulerDeviceType('GEN-TYPE-01', '3'),
+        testlist: 'smoke',
+      },
+    },
+    {
+      id: '4',
+      allDay: false,
+      title: 'base DEV-TYPE-01 and GEN-TYPE-02',
+      backgroundColor: 'darkCyan',
+      overlap: true,
+      rrule: {
+        dtstart: '2020-07-07T07:00:00',
+        byweekday: ['tu'],
+        freq: 'weekly',
+        interval: 1,
+      },
+      duration: '06:30',
+      scheduler: {
+        device: new SchedulerDeviceType('DEV-TYPE-01', '4'),
+        generator: new SchedulerDeviceType('GEN-TYPE-02', '5'),
+        testlist: 'base',
+      },
     },
   ] as CalendarEvent[];
 }
--- server/models/event.ts
+++ server/models/event.ts
@@ -5,14 +5,35 @@ export class CalendarEventRRule {
   dtstart: string;
 }
 
+export class SchedulerDevice {
+  constructor(public name: string, public id: string) {}
+  public type = 'device';
+}
+
+export class SchedulerDeviceType {
+  constructor(public name: string, public id: string) {}
+  public type = 'deviceType';
+}
+
+export class CalendarEventScheduler {
+  usePC: boolean = false;
+  pc?: SchedulerDevice;
+  device: SchedulerDevice | SchedulerDeviceType;
+  generator: SchedulerDevice | SchedulerDeviceType;
+  testlist: string;
+}
+
 export class CalendarEvent {
   // tslint:disable-next-line: variable-name
   id: string;
   title: string;
   allDay: boolean;
   backgroundColor?: string;
+  textColor?: string;
+  color?: string;
   overlap: boolean;
   display?: string;
   rrule?: CalendarEventRRule;
   duration?: string;
+  scheduler?: CalendarEventScheduler;
 }
